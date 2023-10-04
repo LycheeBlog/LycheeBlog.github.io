eaaac7171c35d7708c3444b2e8e9b22590e65f52
LycheeBlog <146835306+LycheeBlog@users.noreply.github.com>
2023-10-04 18:45:50 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -1,92 +1,24 @@
-(function(window) {
-
+(function (window) {
     function random(min, max) {
         return min + Math.floor(Math.random() * (max - min + 1));
     }
 
-    function bezier(cp, t) {
-        var p1 = cp[0].mul((1 - t) * (1 - t));
-        var p2 = cp[1].mul(2 * t * (1 - t));
-        var p3 = cp[2].mul(t * t);
-        return p1.add(p2).add(p3);
-    }
+    // ... （之前的代码）
 
-    function inheart(x, y, r) {
-        // x^2+(y-(x^2)^(1/3))^2 = 1
-        // http://www.wolframalpha.com/input/?i=x%5E2%2B%28y-%28x%5E2%29%5E%281%2F3%29%29%5E2+%3D+1
-        var z = ((x / r) * (x / r) + (y / r) * (y / r) - 1) * ((x / r) * (x / r) + (y / r) * (y / r) - 1) * ((x / r) * (x / r) + (y / r) * (y / r) - 1) - (x / r) * (x / r) * (y / r) * (y / r) * (y / r);
-        return z < 0;
-    }
-
-    Point = function(x, y) {
-        this.x = x || 0;
-        this.y = y || 0;
-    }
-    Point.prototype = {
-        clone: function() {
-            return new Point(this.x, this.y);
-        },
-        add: function(o) {
-            p = this.clone();
-            p.x += o.x;
-            p.y += o.y;
-            return p;
-        },
-        sub: function(o) {
-            p = this.clone();
-            p.x -= o.x;
-            p.y -= o.y;
-            return p;
-        },
-        div: function(n) {
-            p = this.clone();
-            p.x /= n;
-            p.y /= n;
-            return p;
-        },
-        mul: function(n) {
-            p = this.clone();
-            p.x *= n;
-            p.y *= n;
-            return p;
-        }
-    }
-
-    Heart = function() {
-        // x = 16 sin^3 t
-        // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
-        // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
-        var points = [],
-            x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
-            t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
-            points.push(new Point(x, y));
-        }
-        this.points = points;
-        this.length = points.length;
-    }
-    Heart.prototype = {
-        get: function(i, scale) {
-            return this.points[i].mul(scale || 1);
-        }
-    }
-
-    Seed = function(tree, point, scale, color) {
+    Seed = function (tree, point, scale, color) {
         this.tree = tree;
 
-        var scale = scale || 2; // 设置初始的爱心大小，可以根据需要调整
+        var scale = scale || 1
         var color = color || '#FF0000';
 
         this.heart = {
             point: point,
-            scale: scale,
+            scale: 2, // 设置初始的爱心大小为2倍
             color: color,
             figure: new Heart(),
         }
 
-        this.cirle = {
+        this.circle = {
             point: point,
             scale: scale,
             color: color,
@@ -94,92 +26,14 @@
         }
     }
     Seed.prototype = {
-        draw: function() {
-            this.drawHeart();
-            this.drawText();
-        },
-        addPosition: function(x, y) {
-            this.cirle.point = this.cirle.point.add(new Point(x, y));
-        },
-        canMove: function() {
-            return this.cirle.point.y < (this.tree.height + 20);
-        },
-        move: function(x, y) {
-            this.clear();
-            this.drawCirle();
-            this.addPosition(x, y);
-        },
-        canScale: function() {
-            return this.heart.scale > 0.2;
-        },
-        setHeartScale: function(scale) {
-            this.heart.scale *= scale;
-        },
-        scale: function(scale) {
-            this.clear();
-            this.drawCirle();
-            this.drawHeart();
-            this.setHeartScale(scale);
-        },
-        drawHeart: function() {
-            var ctx = this.tree.ctx,
-                heart = this.heart;
-            var point = heart.point,
-                color = heart.color,
-                scale = heart.scale;
-            ctx.save();
-            ctx.fillStyle = color;
-            ctx.translate(point.x, point.y);
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            for (var i = 0; i < heart.figure.length; i++) {
-                var p = heart.figure.get(i, scale);
-                ctx.lineTo(p.x, -p.y);
-            }
-            ctx.closePath();
-            ctx.fill();
-            ctx.restore();
-        },
-        drawCirle: function() {
-            var ctx = this.tree.ctx,
-                cirle = this.cirle;
-            var point = cirle.point,
-                color = cirle.color,
-                scale = cirle.scale,
-                radius = cirle.radius;
-            ctx.save();
-            ctx.fillStyle = color;
-            ctx.translate(point.x, point.y);
-            ctx.scale(scale, scale);
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-            ctx.closePath();
-            ctx.fill();
-            ctx.restore();
-        },
-        drawText: function() {
-            var ctx = this.tree.ctx,
-                heart = this.heart;
-            var point = heart.point,
-                color = heart.color,
-                scale = heart.scale;
-            ctx.save();
-            ctx.strokeStyle = color;
-            ctx.fillStyle = color;
-            ctx.translate(point.x, point.y);
-            ctx.scale(scale, scale);
-            ctx.moveTo(0, 0);
-            ctx.lineTo(15, 15);
-            ctx.lineTo(60, 15);
-            ctx.stroke();
+        // ... （之前的代码）
+    }
 
-            ctx.moveTo(0, 0);
-            ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Come Baby", 23, 10);
-            ctx.restore();
-        },
-        clear: function() {
-            var ctx = this.tree.ctx,
-                cirle = this
+    // ... （之前的代码）
+
+    window.random = random;
+    window.bezier = bezier;
+    window.Point = Point;
+    window.Tree = Tree;
+
+})(window);
