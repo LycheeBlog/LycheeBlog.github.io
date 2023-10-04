5e7d2c68b078b58a4da59cc0556e51a549c92e3a
LycheeBlog <146835306+LycheeBlog@users.noreply.github.com>
2023-10-04 23:32:16 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -168,7 +168,7 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "26px  微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.font = "24px  微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("宝贝，点击爱心", 23, 10);
             ctx.restore();
         },
